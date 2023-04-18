1b10b02945dd6d7b38e68d6af1fe37d5f0510568
imvikaskashyap <mvikaskashyap@gmail.com>
2023-04-18 15:03:25 +0530

    logos

--- src/components/Partner/index.jsx
+++ src/components/Partner/index.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import './assets';
-import L1 from "../../assets/Life_methods-removebg-preview.png";
-import L2 from "../../assets/logo.png";
-import L3 from "../../assets/wallpaper_world.jpeg";
+import L1 from "../../assets/partner_logo/Damante Perfumes Logo.png";
+import L2 from "../../assets/partner_logo/s2-01__1_-removebg-preview.png";
+import L3 from "../../assets/partner_logo/Logo designs (1).png";
+import L4 from "../../assets/partner_logo/logo hcdc.png";
+import L5 from "../../assets/partner_logo/Olip Lighting Logo.png";
+import L6 from "../../assets/partner_logo/Etsy_Profile_Image-removebg-preview.png";
+import L7 from "../../assets/partner_logo/WW_logo-removebg-preview.png";
 
 import "./index.css";
 import Line from "../Line";
 export default function index() {
 	return (
 		<div className="main_container">
-			{/* <div className="filter_container">
-				<li to="/">All</li>
-				<li to="/">Auto & tech</li>
-				<li to="/">Beauty</li>
-				<li to="/">Fashion</li>
-				<li to="/">Other</li>
-				<li to="/">Ott</li>
-				<li to="/">Travel</li>
-			</div> */}
 			<p>Awesome people we work with</p>
 			<h1>Our Clientele</h1>
 			<Line />
@@ -30,42 +25,38 @@ export default function index() {
 				</div>
 
 				<div className="image">
-					<a to="https://www.thefuelmedia.co/" target={"_blank"}>
+					<a to="#" target={"_blank"}>
 						<img src={L2} alt="logo" />
 					</a>
 				</div>
-				{/* <div className="image">
-					<a href="https://www.wallpaperworld.co.in/" target={"_blank"}>
-						<img src={L3} alt="logo" />
-					</a>
-				</div> */}
+
 				<div className="image">
 					<a to="https://www.lifemethods.in/" target={"_blank"}>
-						<img src={L1} alt="logo" />
+						<img src={L3} alt="logo" />
 					</a>
 				</div>
 
 				<div className="image">
 					<a to="https://www.thefuelmedia.co/" target={"_blank"}>
-						<img src={L2} alt="logo" />
+						<img src={L4} alt="logo" />
 					</a>
 				</div>
 				<div className="image">
 					<a to="https://www.lifemethods.in/" target={"_blank"}>
-						<img src={L1} alt="logo" />
+						<img src={L5} alt="logo" />
 					</a>
 				</div>
 
 				<div className="image">
 					<a to="https://www.thefuelmedia.co/" target={"_blank"}>
-						<img src={L2} alt="logo" />
+						<img src={L6} alt="logo" />
 					</a>
 				</div>
-				{/* <div className="image">
+				<div className="image">
 					<a href="https://www.wallpaperworld.co.in/" target={"_blank"}>
-						<img src={L3} alt="logo" />
+						<img src={L7} alt="logo" />
 					</a>
-				</div> */}
+				</div>
 			</div>
 		</div>
 	);
